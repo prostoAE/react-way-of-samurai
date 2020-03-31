241c40b2aaa40c42db51e376f2f9afe3b9654c79
prostoAE <trident998@gmail.com>
2020-03-31 13:40:15 +0300

    structured

---
+++ src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div><img src="https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="foto"/></div>
+            <div>ava + description</div>
+            <div>My posts
+                <div>new post</div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+        </div> 
+    );
+}
+
+export default Profile;
