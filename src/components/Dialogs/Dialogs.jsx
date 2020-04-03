0858096723ec4e42c2c1cd0686c1ccc59580517f
tride <tride@DESKTOP-JCBSPBJ>
2020-04-03 12:16:38 +0300

    less 18

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Dialogs.module.css';
+
+const Dialogs = (props) => {
+    return (
+        <div className={s.content}>Dialogs</div>
+    )
+};
+
+export default Dialogs;
