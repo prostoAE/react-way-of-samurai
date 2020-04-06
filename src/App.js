29dfb355b609490410968c80beb899c857c15e8a
prostoAE <trident998@gmail.com>
2020-04-06 10:29:24 +0300

    less 19

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
+import { Route, BrowserRouter } from 'react-router-dom';
 
 function App() {
     return (
-        <div className="app-wrapper">
-            <Header/>
-            <div className="row">
-                <Navbar/>
-                {/*<Profile/>*/}
-                <Dialogs/>
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <div className="row">
+                    <Navbar/>
+                    <Route path="/profile" component={Profile} />
+                    <Route path="/dialogs" component={Dialogs} />
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,8 +5,8 @@ const Navbar = () => {
     return (
         <aside className={s.nav}>
             <ul>
-                <li className={`${s.item} ${s.active}`}><a href="#">Profile</a></li>
-                <li className={s.item}><a href="#">Messages</a></li>
+                <li className={`${s.item} ${s.active}`}><a href="/profile">Profile</a></li>
+                <li className={s.item}><a href="/dialogs">Messages</a></li>
                 <li className={s.item}><a href="#">News</a></li>
                 <li className={s.item}><a href="#">Music</a></li>
             </ul>
