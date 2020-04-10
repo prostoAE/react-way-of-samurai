07b9ea5b38be3f4b8907262d0bc09e800996caa8
tride <tride@DESKTOP-JCBSPBJ>
2020-04-10 17:07:37 +0300

    less 21

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,7 +3,20 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div className={s.content}>Dialogs</div>
+        <div className={s.content}>
+            <ul className={s.dialoglist}>
+                <li className={s.dialogitem + ' ' + s.active}>Andrey</li>
+                <li className={s.dialogitem}>Petr</li>
+                <li className={s.dialogitem}>Fedya</li>
+                <li className={s.dialogitem}>Stepan</li>
+                <li className={s.dialogitem}>Valerij</li>
+            </ul>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>Hello</div>
+                <div className={s.message}>Hou are you!</div>
+            </div>
+        </div>
     )
 };
 
