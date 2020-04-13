92d660dc33636e582efa9d3c12e1403d040887be
tride <tride@DESKTOP-JCBSPBJ>
2020-04-13 11:23:45 +0300

    less 26

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
-import { Route, BrowserRouter } from 'react-router-dom';
 
 function App() {
     return (
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,20 +1,7 @@
 import React from 'react';
 import s from './Dialogs.module.css';
-import {NavLink} from 'react-router-dom';
-
-const DiaolgItem = (props) => {
-    let path = '/dialogs/' + props.id;
-
-    return (
-        <li className={s.dialogitem + ' ' + s.active}>
-            <NavLink to={path}>{props.name}</NavLink>
-        </li>
-    )
-};
-
-const Message = (props) => {
-    return <div className={s.message}>{props.message}</div>
-};
+import Message from './Message/Message';
+import DialogItem from './DialogItem/DialogItem';
 
 const Dialogs = (props) => {
 
@@ -36,7 +23,7 @@ const Dialogs = (props) => {
         {id: 6, message: 'Yes. Off cose'}
     ];
 
-    let dialogsElements = dialogs.map(d => <DiaolgItem name={d.name} id={d.id}/>);
+    let dialogsElements = dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
     let messagesElements = messages.map(m => <Message message={m.message}/>);
 
     return (
