21413d83e0542716747c2df7795901ccb2c79f8a
tride <tride@DESKTOP-JCBSPBJ>
2020-04-14 16:05:58 +0300

    less 33

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
 import state from './redux/state';
-import {addPost} from './redux/state';
+import {rerenderEntireTree} from './render';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App state={state} addPost={addPost} />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+rerenderEntireTree(state);
+
 
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,5 @@
+import {rerenderEntireTree} from '../render';
+
 let state = {
     profilePage: {
         posts: [
@@ -36,6 +38,7 @@ export let addPost = (postMessage) => {
     };
 
     state.profilePage.posts.push(newPost);
+    rerenderEntireTree(state);
 };
 
 export default state;
