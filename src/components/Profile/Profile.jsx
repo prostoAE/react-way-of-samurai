4ee508714179b70c4f91266e0f9c3829ae62e378
tride <tride@DESKTOP-JCBSPBJ>
2020-04-17 16:50:06 +0300

    less 38

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -9,9 +9,7 @@ const Profile = (props) => {
             <ProfileInfo/>
             <MyPosts posts={props.profilePage.posts}
                      newPostText={props.profilePage.newPostText}
-                     addPost={props.addPost}
-                     updateNewPostText={props.updateNewPostText}
-            />
+                     dispatch={props.dispatch} />
         </div>
     );
 };
