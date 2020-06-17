b59ed52b8c247271aedd642a689a7ef920f42d9f
tride <tride@DESKTOP-JCBSPBJ>
2020-06-17 12:29:29 +0300

    less 55 - pagination, постраничный вывод пользователей

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -6,14 +6,48 @@ import userPhoto from '../../assets/images/user-default.png'
 class Users extends Component {
 
     componentDidMount() {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.totalUsersCount}`)
         .then(response => {
             this.props.setUsers(response.data.items);
+            // this.props.setTotalCount(response.data.totalCount); //Вызывает ошибку из-за большого обьема данных (> 100)
+            this.props.setTotalCount(40);
         });
     }
 
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.totalUsersCount}`)
+        .then(response => {
+            this.props.setUsers(response.data.items);
+        });
+    };
+
     render() {
+
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+        let pages = [];
+
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i);
+        }
+
         return <div className={s.content}>
+            <ul className={s.pagination}>
+                {pages.map(page => {
+                    return <li className={this.props.currentPage === page ? s.active : ''} key={page}>
+                        <a href="#" onClick={(e) => {
+                            e.preventDefault();
+                            this.onPageChanged(page)
+                        }}>
+                            {page}
+                        </a>
+                    </li>
+                })}
+
+                {/*<li className={s.active}><a href="#">2</a></li>*/}
+
+            </ul>
             {
                 this.props.users.map(user => <div key={user.id}>
                 <span>
