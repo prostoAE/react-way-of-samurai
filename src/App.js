8f38bf63a4dc14a26c19aaa2ad58ca5037bf192e
prostoAE <trident998@gmail.com>
2021-01-15 11:11:27 +0200

    68 - React JS - Redirect

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainer from "./components/Header/HeaderContainer";
+import LoginPage from "./components/login/Login";
 
 function App() {
     return (
@@ -13,12 +14,10 @@ function App() {
             <HeaderContainer/>
             <div className="row">
                 <Navbar/>
-                <Route path="/profile/:userId?" render={() => <ProfileContainer/>
-                }/>
-                <Route path="/dialogs" render={() => <DialogsContainer/>
-                }/>
-                <Route path="/users" render={() => <UsersContainer/>
-                }/>
+                <Route path="/profile/:userId?" render={() => <ProfileContainer/>}/>
+                <Route path="/dialogs" render={() => <DialogsContainer/>}/>
+                <Route path="/users" render={() => <UsersContainer/>}/>
+                <Route path="/login" render={() => <LoginPage/>}/>
             </div>
         </div>
     );
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import s from './Dialogs.module.css';
 import Message from './Message/Message';
 import DialogItem from './DialogItem/DialogItem';
+import {Redirect} from "react-router-dom";
 
 const Dialogs = (props) => {
 
@@ -23,6 +24,8 @@ const Dialogs = (props) => {
         let body = e.target.value;
         props.updateNewMessageBody(body);
     };
+
+    if (!props.isAuth) return <Redirect to={"/login"}/>;
 
     return (
         <div className={s.content}>
