0c953e3e39492ec013f3e4ff6c283def7f19eb6b
prostoAE <trident998@gmail.com>
2021-01-28 10:11:04 +0200

    74 - React JS - componentDidUpdate

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -21,6 +21,14 @@ class ProfileStatus extends React.Component {
         this.props.updateStatus(this.state.status);
     };
 
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status
+            })
+        }
+    }
+
     onStatusChange = (e) => {
         this.setState({
                 status: e.currentTarget.value
